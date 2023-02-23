db315c20653afce57164851a10bc8543e0d9801f
Beatriz <beatrizfreitasti@gmail.com>
2023-02-23 16:10:59 -0300

    Criando e inserindo na tabela Médico

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -20,12 +20,13 @@ import med.voll.api.endereco.Endereco;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Medico {
-    
+
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome; 
-    private String email; 
+    private String email;
+    private String telefone; 
     private String crm; 
     
     @Enumerated
@@ -37,6 +38,7 @@ public class Medico {
     public Medico(DadosCadastroMedico dados){
         this.nome = dados.nome();
         this.email = dados.email();
+        this.telefone = dados.telefone();
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
